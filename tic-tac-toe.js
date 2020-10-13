40dd68ebdbc29ee5e06c89620acc57446fe05084
Jaun Luc Brown <jaunlucbrown15@gmail.com>
2020-10-12 20:06:08 -0500

    Hover implemented

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,18 +6,26 @@ window.addEventListener('load' , ()=>{
     var x;
     for(x =0; x < $c.length; x++){
         $c[x].className='square';
-        $c[x].addEventListener('click', game)
+        $c[x].addEventListener('click', game);
+        $c[x].addEventListener('mouseover', cursor);
+        $c[x].addEventListener('mouseout', nocursor);
     }
 
+    var $track = [ '' , '' , '' , '' , '', '' , '' , '' , ''];
     const $plays = Array.from($c);
 
     function game(e){
         const $box = $plays.indexOf(e.target);
         $player == 1 ? (e.target.innerHTML = $pX, e.target.classList.add("X"), $player=2) :
-        $player == 2 ? (e.target.innerHTML = $pO, e.target.classList.add("O"), $player=1) : alert("Error")
+        $player == 2 ? (e.target.innerHTML = $pO, e.target.classList.add("O"), $player=1) : alert("Error");
     }
 
-
+    function cursor(e){
+        e.target.classList.add("hover");
+    }
+    function nocursor(e){
+        e.target.classList.remove("hover");
+    }
 
 
 })
