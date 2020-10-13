20271f3d13cac4d697ff27e63c861bf55f12d004
Jaun Luc Brown <jaunlucbrown15@gmail.com>
2020-10-12 19:29:18 -0500

    Click implemented

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,23 @@
-window.addEventListener("load", function(){ 
+window.addEventListener('load' , ()=>{
 
-let c = document.getElementById("board").children;
-console.log(c);
+    var $pX = 'X', $pO = 'O', $player = 1 ;
 
-for (var bx = 0; bx <= c.length ; bx++){
-    c[bx].className="square";
-}
+    var $c = document.getElementById("board").children;
+    var x;
+    for(x =0; x < $c.length; x++){
+        $c[x].className='square';
+        $c[x].addEventListener('click', game)
+    }
 
-});
+    const $plays = Array.from($c);
+
+    function game(e){
+        const $box = $plays.indexOf(e.target);
+        $player == 1 ? (e.target.innerHTML = $pX, e.target.classList.add("X"), $player=2) :
+        $player == 2 ? (e.target.innerHTML = $pO, e.target.classList.add("O"), $player=1) : alert("Error")
+    }
+
+
+
+
+})
